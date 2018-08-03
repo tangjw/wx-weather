e304d5f9a1ebd7ba4badd9066ca64d8aa787d2fc
tangjw <tjwabc@gmail.com>
2018-08-03 17:19:53 +0800

    设置

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -5,8 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    screenBrightness:50,
-
+    screenBrightness: '50',
+    setting: { hiddenIndex:false},
+    enableUpdate:true,
   },
 
   /**
@@ -20,14 +21,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+    console.log("setting onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    console.log("setting onShow")
+    this.getScreenBrightness()
   },
 
   /**
@@ -66,26 +68,70 @@ Page({
   },
   switchChange(e) {
     console.log(e)
+    let dataset = e.currentTarget.dataset
+    let switchparam = dataset.switchparam
+    let setting = this.data.setting
+    if (switchparam === 'hiddenIndex') {
+      if (this.data.enableUpdate) {
+        setting[switchparam] = !e.detail.value
+        wx.showToast({
+          title: '设置成功',
+          icon: 'none',
+          duration: 2000,
+        })
+      } else {
+        setting[switchparam] = false
+        wx.showToast({
+          title: '无法使用该功能',
+          icon: 'none',
+          duration: 2000,
+        })
+      }
+    } 
+
+    this.setData({
+      setting,
+    })
+    wx.setStorage({
+      key: 'setting',
+      data: setting,
+    })
   },
-  screenBrightnessChange(e){
-   
+  screenBrightnessChange(e) {
+
     this.setScreenBrightness(e.detail.value)
   },
 
   screenBrightnessChanging(e) {
-   
+
     this.setScreenBrightness(e.detail.value)
   },
   setScreenBrightness(val) {
     let that = this
     wx.setScreenBrightness({
       value: val / 100,
-      success: function (res) {
+      success: function(res) {
         that.setData({
           screenBrightness: val,
         })
       },
     })
   },
-  
+  //获取当前亮度
+  getScreenBrightness() {
+    let that = this
+    wx.getScreenBrightness({
+      success: function(res) {
+        that.setData({
+          screenBrightness: Number(res.value * 100).toFixed(0),
+        })
+      },
+      fail: function(res) {
+        that.setData({
+          screenBrightness: '50',
+        })
+      },
+    })
+  },
+
 })
